50929268b389bdd7927dd81d20b1d879c4373d32
JLOverlordU <U22320490@utp.edu.pe>
2025-09-18 02:01:37 -0500

    feat: agregar el template base de Vue 2

---
+++ src/store.js
@@ -0,0 +1,35 @@
+import Vue from 'vue'
+import Vuex from 'vuex'
+Vue.use(Vuex)
+
+const state = {
+  sidebarShow: 'responsive',
+  sidebarMinimize: false,
+  asideShow: false,
+  darkMode: false,
+  // url: 'https://erp-back.nutri-van.com/api/',
+  // url: 'https://backend.nutri-van.com/api/',
+  url: 'http://127.0.0.1:8000/api/',
+}
+
+const mutations = {
+  toggleSidebarDesktop (state) {
+    const sidebarOpened = [true, 'responsive'].includes(state.sidebarShow)
+    state.sidebarShow = sidebarOpened ? false : 'responsive'
+  },
+  toggleSidebarMobile (state) {
+    const sidebarClosed = [false, 'responsive'].includes(state.sidebarShow)
+    state.sidebarShow = sidebarClosed ? true : 'responsive'
+  },
+  set (state, [variable, value]) {
+    state[variable] = value
+  },
+  toggle (state, variable) {
+    state[variable] = !state[variable]
+  }
+}
+
+export default new Vuex.Store({
+  state,
+  mutations
+})
